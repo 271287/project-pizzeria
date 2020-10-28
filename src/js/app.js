cbba20da4f4783000604944757f02024e98f3f62
Natalia Szymiczek <nat.szymiczek@gmail.com>
2020-10-28 19:54:39 +0100

    10.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
-import {settings, select, classNames} from './settings.js';
-import Product from './components/Product.js';
-import Cart from './components/Cart.js';
-import Booking from './components/Booking.js';
+import {settings, select, classNames} from '../settings.js';
+import Product from './Product.js';
+import Cart from './Cart.js';
+import Booking from './Booking.js';
 
 const app = {
   initBooking: function(){
@@ -12,41 +12,33 @@ const app = {
 
   },
 
-  initPages: function () {
+  initPages: function() {
     const thisApp = this;
 
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
-    thisApp.navLinks = document.querySelectorAll(select.navLinks);
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
-    const idFromHash = window.location.hash.replace('#', '');
-    //console.log('idFromHash', idFromHash);
-
+    const idFromHash = window.location.hash.replace('#/', '');
     let pageMatchingHash = thisApp.pages[0].id;
 
-    for(let page of thisApp.pages){
-      if(page.id == idFromHash){
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
         pageMatchingHash = page.id;
         break;
       }
     }
 
-    thisApp.activatePage(thisApp.pages[0].id);
+    thisApp.activatePage(pageMatchingHash);
 
-    for(let link of thisApp.navLinks){
-      link.addEventListener('click', function(event){
+    for (let link of thisApp.navLinks)
+      link.addEventListener('click', function(event) {
         const clickedElement = this;
         event.preventDefault();
 
-        /*get page id from href attribute */
-        const id = clickedElement.getAttribute('href').replace('#', '');
-
-        /*run thisApp.activatePage with that id */
+        let id = clickedElement.getAttribute('href').replace('#', '');
         thisApp.activatePage(id);
-
-        /*change URL hush*/
         window.location.hash = '#/' + id;
       });
-    }
   },
 
   activatePage: function (pageId) {
--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,14 +1,32 @@
-class Booking {
-  constructor(){
+import { select, templates } from '../settings.js';
+import AmountWidget from './AmountWidget.js';
 
+class Booking {
+  constructor(element){
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
   } 
   
-  render(){
+  render(element){
+    const thisBooking = this;
+
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
 
   }
   
   initwidgets(){
+    const thisBooking = this;
 
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
   }
 }
+
 export default Booking;
