244576e32e162e954938f01edaf627f6eea4f42c
Natalia Szymiczek <nat.szymiczek@gmail.com>
2020-10-30 18:24:07 +0100

    10.3

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -18,14 +18,15 @@ class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
-
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
   }
   
-  initwidgets(){
+  initWidgets(){
     const thisBooking = this;
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new AmountWidget(thisBooking.dom.datePicker);
   }
 }
 
